64f9e7646834c83e30aeabae5409d27972aaca19
Cooper Geng <cooper.geng@fuhu.com>
2015-07-05 21:54:14 -0700

    one more solution to invert binart tree

---
+++ examples/InvertBinaryTree/InvertBinaryTree2.java
@@ -0,0 +1,31 @@
+class InvertBinaryTree2 {
+	
+	public TreeNode invertTree(TreeNode root) {
+		LinkedList<TreeNode> queue = new LinkedList<TreeNode>();
+		if (root!=null) {
+			queue.add(root);
+		}
+		
+		while(!queue.isEmpty()){
+			TreeNode p = queue.poll();
+			if (p.left!=null) {
+				queue.add(p.left);
+			}
+			if (p.right!=null) {
+				queue.add(p.right);
+			}
+			
+			TreeNode temp = p.left;
+			p.left = p.right;
+			p.right = temp;
+		}
+	}
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println();		
+	}
+}
