c4138e2681823cdac7d54c231ae79707261965bf
Cooper Geng <cooper.geng@fuhu.com>
2015-07-05 23:10:14 -0700

    rename folder

--- examples/InvertBinaryTree/InvertBinaryTree.java
+++ examples/InvertBinaryTree/InvertBinaryTree.java
@@ -21,7 +21,7 @@ class InvertBinaryTree {
 		}
 		
 	}
-	
+
 	
 	public static void main(String[] args) {
 		
--- examples/height_BST/topic.md
+++
@@ -1,3 +0,0 @@
-Maximum Height (Depth) of a Binary Tree
-
-* Given a binary tree, find its maximum height.
